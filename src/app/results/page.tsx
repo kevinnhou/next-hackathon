9f4ef46682c8373583ebad6416d5b7d00a4fe760
Oscar B <oscar.boys2@outlook.com>
2025-04-16 17:58:52 +1000

    feat: update results page layout and functionality
    - Changed the UI to a 'podium' on desktop
    - Made mobile view remain as the stacked cards view to maintain clarity
    - Changed the title from 'Top Picks' to 'Voting Results' for clarity.
    - Introduced a new component for displaying all restaurants and vetoed options (a big list)
    - Introduced 'Undo' to individual vetoed items

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -2,8 +2,8 @@ import { Results } from "@/components/results/results";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-4 md:p-24">
-      <h1 className="mb-8 text-3xl font-bold">Top Picks</h1>
+    <main className="flex min-h-screen flex-col items-center p-4 pb-12">
+      <h1 className="mt-6 mb-8 text-3xl font-bold">Voting Results</h1>
       <Results />
     </main>
   );
